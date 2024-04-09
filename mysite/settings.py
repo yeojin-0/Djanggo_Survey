f14c64a3409bb768faafdcda2a0b2e0e849192fc
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 14:57:08 +0900

    환경 변수세팅(.env)

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+# load .env
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qkvtc8%$xo0$w-vhlg&um%je@&wo(u*nqn#o8y@4r$qovkh=d("
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
